b1c37d38ecf2cc6ca9cc15aa48d6a15e8e17ab8c
aye, kyi lei <kaye2@go.pasadena.edu>
2023-10-12 22:46:06 -0700

    some changes

--- personal.js
+++ personal.js
@@ -25,3 +25,17 @@ popupTrigger2.addEventListener("click", () => {
 popupClose2.addEventListener("click", () => {
     popup2.style.display = "none";
 });
+
+const popupTrigger3 = document.getElementById("popup-trigger3");
+const popupClose3 = document.getElementById("popup-close3");
+const popup3 = document.getElementById("popup3");
+
+// Show pop-up when trigger is clicked
+popupTrigger3.addEventListener("click", () => {
+    popup3.style.display = "block";
+});
+
+// Close pop-up when close button is clicked
+popupClose3.addEventListener("click", () => {
+    popup3.style.display = "none";
+});
